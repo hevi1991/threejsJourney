e2d42c8a736b94c06981bbc490869722758bd3fb
hewei <he.wei@email.cn>
2021-07-22 16:51:23 +0800

    e19

---
+++ 19-raycaster/src/script-raycast-in-animate.js
@@ -0,0 +1,135 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+import { AxesHelper } from "three";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+const axesHelper = new AxesHelper(3);
+scene.add(axesHelper);
+
+/**
+ * Objects
+ */
+const object1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: "#ff0000" })
+);
+object1.position.x = -2;
+
+const object2 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: "#ff0000" })
+);
+
+const object3 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: "#ff0000" })
+);
+object3.position.x = 2;
+
+scene.add(object1, object2, object3);
+
+/**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster();
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
+camera.position.z = 5;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+
+    // Animate objects
+    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
+    // cast a ray 投个射线
+    const rayOrigin = new THREE.Vector3(-3, 0, 0);
+    const rayDirection = new THREE.Vector3(1, 0, 0).normalize();
+
+    raycaster.set(rayOrigin, rayDirection);
+    const objects2Test = [object1, object2, object3];
+    const intersects = raycaster.intersectObjects(objects2Test);
+
+    for (const object of objects2Test) {
+        object.material.color.set("#ff0000");
+    }
+
+    for (const intersect of intersects) {
+        intersect.object.material.color.set("#0000ff");
+    }
+
+    // Update controls
+    controls.update();
+
+    // Render
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+};
+
+tick();
---
+++ 19-raycaster/src/script.js
@@ -0,0 +1,173 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+import { AxesHelper } from "three";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+const axesHelper = new AxesHelper(3);
+scene.add(axesHelper);
+
+/**
+ * Objects
+ */
+const object1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: "#ff0000" })
+);
+object1.position.x = -2;
+
+const object2 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: "#ff0000" })
+);
+
+const object3 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: "#ff0000" })
+);
+object3.position.x = 2;
+
+scene.add(object1, object2, object3);
+
+/**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster();
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Mouse
+ * 鼠标对应scene中的二维坐标
+ *   1
+ * -101
+ *  -1
+ */
+const mouse = new THREE.Vector2();
+window.addEventListener("mousemove", (event) => {
+    mouse.x = (event.clientX / sizes.width) * 2 - 1;
+    mouse.y = -(event.clientY / sizes.height) * 2 + 1;
+});
+window.addEventListener("click", (event) => {
+    if (currentIntersect) {
+        if (currentIntersect.object === object1) {
+            console.log("click on a sphere 1");
+        } else if (currentIntersect.object === object2) {
+            console.log("click on a sphere 2");
+        } else if (currentIntersect.object === object3) {
+            console.log("click on a sphere 3");
+        }
+    }
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
+camera.position.z = 5;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+let currentIntersect = null;
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+
+    // Animate objects
+    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
+    // cast a ray
+    raycaster.setFromCamera(mouse, camera); // 从鼠标对应的坐标位置与镜头发射一条射线
+
+    const objects2Test = [object1, object2, object3];
+    const intersects = raycaster.intersectObjects(objects2Test);
+
+    for (const object of objects2Test) {
+        object.material.color.set("#ff0000");
+    }
+
+    for (const intersect of intersects) {
+        intersect.object.material.color.set("#0000ff");
+    }
+
+    if (intersects.length) {
+        // something being hovered
+        if (currentIntersect === null) {
+            // mouse enter
+        }
+        currentIntersect = intersects[0]; // 拿与射线源最近的一个点
+    } else {
+        // nothing being hovered
+        if (currentIntersect) {
+            // mouse leave
+        }
+        currentIntersect = null;
+    }
+
+    // Update controls
+    controls.update();
+
+    // Render
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+};
+
+tick();
