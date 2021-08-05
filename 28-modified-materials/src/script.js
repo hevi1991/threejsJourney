8373e2d23cc1119943ad8dc2e1010ce9d1b0e2d1
hewei <he.wei@email.cn>
2021-08-05 18:02:52 +0800

    e28

---
+++ 28-modified-materials/src/script.js
@@ -0,0 +1,265 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import * as dat from "dat.gui";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Loaders
+ */
+const textureLoader = new THREE.TextureLoader();
+const gltfLoader = new GLTFLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+/**
+ * Update all materials
+ */
+const updateAllMaterials = () => {
+    scene.traverse((child) => {
+        if (
+            child instanceof THREE.Mesh &&
+            child.material instanceof THREE.MeshStandardMaterial
+        ) {
+            child.material.envMapIntensity = 5;
+            child.material.needsUpdate = true;
+            child.castShadow = true;
+            child.receiveShadow = true;
+        }
+    });
+};
+
+/**
+ * Environment map
+ */
+const environmentMap = cubeTextureLoader.load([
+    "/textures/environmentMaps/0/px.jpg",
+    "/textures/environmentMaps/0/nx.jpg",
+    "/textures/environmentMaps/0/py.jpg",
+    "/textures/environmentMaps/0/ny.jpg",
+    "/textures/environmentMaps/0/pz.jpg",
+    "/textures/environmentMaps/0/nz.jpg",
+]);
+environmentMap.encoding = THREE.sRGBEncoding;
+
+scene.background = environmentMap;
+scene.environment = environmentMap;
+
+/**
+ * Material
+ */
+
+// Textures
+const mapTexture = textureLoader.load("/models/LeePerrySmith/color.jpg");
+mapTexture.encoding = THREE.sRGBEncoding;
+
+const normalTexture = textureLoader.load("/models/LeePerrySmith/normal.jpg");
+
+// Material
+const material = new THREE.MeshStandardMaterial({
+    map: mapTexture,
+    normalMap: normalTexture,
+});
+
+// 实例化深度材质给模型用
+const deptMaterial = new THREE.MeshDepthMaterial({
+    depthPacking: THREE.RGBADepthPacking,
+});
+
+const customUniforms = {
+    uTime: { value: 0 },
+};
+
+material.onBeforeCompile = (shader, render) => {
+    // 复写模型自带的vertexShader行为 ./model-vertex-sample.txt 是shader.vertexShader对象信息，可以用作参考。
+    shader.uniforms.uTime = customUniforms.uTime;
+
+    shader.vertexShader = shader.vertexShader.replace(
+        "#include <common>",
+        `
+        uniform float uTime;
+
+        #include <common>
+        
+        mat2 get2dRotateMatrix(float _angle){
+            return mat2(cos(_angle),-sin(_angle),
+                        sin(_angle),cos(_angle));
+        }
+        `
+    );
+    shader.vertexShader = shader.vertexShader.replace(
+        "#include <beginnormal_vertex>",
+        `
+        #include <beginnormal_vertex>
+
+        float angle = sin((position.y + uTime) * 0.4);
+        mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+        objectNormal.xz = rotateMatrix * objectNormal.xz;
+        `
+    );
+    shader.vertexShader = shader.vertexShader.replace(
+        "#include <begin_vertex>",
+        `
+        #include <begin_vertex>
+
+        transformed.xz = rotateMatrix * transformed.xz;
+        `
+    );
+};
+deptMaterial.onBeforeCompile = (shader) => {
+    // 复写自定义的深度材质vertexShader行为
+    shader.uniforms.uTime = customUniforms.uTime;
+
+    shader.vertexShader = shader.vertexShader.replace(
+        "#include <common>",
+        `
+        uniform float uTime;
+
+        #include <common>
+        
+        mat2 get2dRotateMatrix(float _angle){
+            return mat2(cos(_angle),-sin(_angle),
+                        sin(_angle),cos(_angle));
+        }
+        `
+    );
+
+    shader.vertexShader = shader.vertexShader.replace(
+        "#include <begin_vertex>",
+        `
+        #include <begin_vertex>
+        float angle = sin((position.y + uTime) * 0.4);
+        mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+        transformed.xz = rotateMatrix * transformed.xz;
+        `
+    );
+};
+
+/**
+ * Models
+ */
+gltfLoader.load("/models/LeePerrySmith/LeePerrySmith.glb", (gltf) => {
+    // Model
+    const mesh = gltf.scene.children[0];
+    mesh.rotation.y = Math.PI * 0.5;
+    mesh.material = material;
+    mesh.customDepthMaterial = deptMaterial; // 渲染到深度贴图时此材质要使用的自定义深度材质。如果用了这个，会覆盖原有行为
+    scene.add(mesh);
+
+    // Update materials
+    updateAllMaterials();
+});
+
+/**
+ * Plane
+ */
+const plane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(15, 15, 15),
+    new THREE.MeshStandardMaterial()
+);
+plane.rotation.y = Math.PI;
+plane.position.y = -5;
+plane.position.z = 5;
+scene.add(plane);
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.normalBias = 0.05;
+directionalLight.position.set(0.25, 2, -2.25);
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
+camera.position.set(8, 2, -8);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
+renderer.physicallyCorrectLights = true;
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+
+    // Update material
+    customUniforms.uTime.value = elapsedTime;
+
+    // Update controls
+    controls.update();
+
+    // Render
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+};
+
+tick();
